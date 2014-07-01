9110e39b59b0fa6344a54ee4eadb20320b36b5e7
Budhram Gurung <budhrg@gmail.com>
2014-07-01 23:26:24 +0530

    Updated method demo code

--- course_day2/12.method_demo.rb
+++ course_day2/12.method_demo.rb
@@ -12,9 +12,19 @@ def square(n)
   n * n
 end
 
+def cube(n)
+  n * n * n
+end
+
 puts '==========> 1. transform! : Method way'
 array = [1, 2, 3, 4]
 puts "Before transform! : #{array.inspect}"
 my_sqr = method(:square)
 array.transform!(my_sqr)
 puts "After transform! as square : #{array.inspect}"
+
+array = [1, 2, 3, 4]
+puts "Before transform! : #{array.inspect}"
+my_cube = method(:cube)
+array.transform!(my_cube)
+puts "After transform! as cube : #{array.inspect}"
