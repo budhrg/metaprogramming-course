9ee16d9d9427bb7617549371e2bd5b9430790db8
Budhram Gurung <budhram.gurung01@gmail.com>
2014-07-08 15:40:09 +0530

    Updated delegator to do chainig of results

--- course_day3/ror_method_missing_demo/app/models/product.rb
+++ course_day3/ror_method_missing_demo/app/models/product.rb
@@ -2,10 +2,20 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :product_id
 
   def self.search(params)
-    products = DynamicDelegator.new(scoped)
-    products = products.where("name like ?", "%" + params[:name] + "%") if params[:name]
-    products = products.where("price > ?", params[:price_gt]) if params[:price_gt]
-    products = products.where("price < ?", params[:price_lt]) if params[:price_lt]
+    products = scope_builder
+    products.where("name like ?", "%" + params[:name] + "%") if params[:name]
+    products.where("price > ?", params[:price_gt]) if params[:price_gt]
+    products.where("price < ?", params[:price_lt]) if params[:price_lt]
     products
   end
+
+  def self.scope_builder
+    DynamicDelegator.new(scoped)
+  end
 end
+
+# Do like this in Rails Console
+# Product.send(*[:where, "name like ?", "soap"])
+# Product.send(*[:where, "price > ?", "120"])
+# Product.send(*[:where, "price < ?", "120"])
+
--- course_day3/ror_method_missing_demo/lib/dynamic_delegator.rb
+++ course_day3/ror_method_missing_demo/lib/dynamic_delegator.rb
@@ -1,10 +1,16 @@
-class DynamicDelegator
+class DynamicDelegator# < BasicObject
   def initialize(target)
     @target = target
   end
 
   def method_missing(*args, &block)
-    @target.send(*args, &block)
+    result = @target.send(*args, &block)
+    @target = result if result.kind_of? @target.class
+    result
   end
 end
+
+# Achieving something like this
+# Product.where("name like ?", "soap")
+#        .where("price > ?", "100")
 
