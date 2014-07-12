041c92b749bec5933743ead3d80467bd622a67ed
Budhram Gurung <budhram.gurung01@gmail.com>
2014-07-13 00:05:09 +0530

    Updated ror demo for method_missing

--- course_day3/ror_method_missing_demo/config/routes.rb
+++ course_day3/ror_method_missing_demo/config/routes.rb
@@ -1,4 +1,4 @@
 RorMethodMissingDemo::Application.routes.draw do
   resources :products
-
+  root to: 'products#index'
 end
--- course_day3/ror_method_missing_demo/db/seeds.rb
+++ course_day3/ror_method_missing_demo/db/seeds.rb
@@ -1,7 +1,7 @@
-10.times.each do |i|
+20.times.each do |i|
   Product.create! do |product|
     product.product_id = i+1
-    product.name = "Product #{i+1}"
+    product.name = i < 10 ? "Soap" : "Detergent"
     product.price  = 20 * (i+1)
     product.description = "Description of Product #{i+1}"
   end
